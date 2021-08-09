e8f31a45078aa10f4f78f88c6973ca48b459524f
BraboOliveira <rafaelgsn@live.com>
2021-08-09 01:48:57 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
+import React,{useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import  './components/table.css'
+const axios = require('axios');
 
 function App() {
+  const [dados, setdados] = useState([])
+  let [dadosLength, setDadosLength] = useState(null);
+  useEffect(() => {
+    const timer1 = setTimeout(() => {
+      axios.get('https://vendebelem.com/php-react/all-users.php')
+    .then(function (response) {
+      // handle success
+      console.log(response.data.users.length);
+      setdados(response.data.users);
+      if(response.data.users.length != 0){
+        setDadosLength(1);
+      }
+      else{
+        setDadosLength(null);
+      }
+      
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+    }
+    , 1000)
+    return () => {
+      clearTimeout(timer1)
+    }
+  }, [])
+
+  const Item =(data)=> {
+    console.log(data);
+    return (
+        <tr>
+          <td>{data.value.id}</td>
+          <td>{data.value.dados1}</td>
+          <td>{data.value.dados2}</td>
+        </tr>
+    );
+ }
+ 
+ const MyList=()=>{
+
+    return (
+    <>
+       {dadosLength === null ? "Nenhum dado encontrado" : dados.map((dados) => <Item key={dados.id} value={dados} />)}
+    </>
+   );
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    < >
+      <table className="table">
+      <caption>INFORMAÇÕES</caption>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Código</th>
+              <th>Data/Hora</th>
+            </tr>
+          </thead>
+      </table>
+      <tbody className="table">
+      {dadosLength != null ?  <MyList/> : "Aguarde obtendo informações..." }
+      </tbody>
+    </>
   );
 }
 
