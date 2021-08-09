726e4c9d6a705c1deadf7bd26ab99743af40cb36
BraboOliveira <paulorafaelbrabo@gmail.com>
2021-08-09 11:30:40 -0300

    Novidades

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import  './components/table.css'
+import { DataGrid } from '@material-ui/data-grid';
 const axios = require('axios');
 
 function App() {
@@ -36,6 +37,34 @@ function App() {
     }
   }, [])
 
+  const columns = [
+  { field: 'id', headerName: 'ID', width: 200 },
+  {
+    field: 'dados1',
+    headerName: 'CÓDIGO',
+    width: 300,
+    editable: true,
+  },
+  {
+    field: 'dados2',
+    headerName: 'DATA/HORA',
+    width: 200,
+    editable: true,
+  },
+];
+
+const rows = [
+  { id: 1, codigo: 'A1XXXXXXXXXXXXXXXE5', data: '05/06/21 11:00', age: 35 },
+  { id: 2, codigo: 'A1XXXXXXXXXXXXXXXE5', data: '05/06/21 11:00', age: 42 },
+  { id: 3, codigo: 'A1XXXXXXXXXXXXXXXE5', data: '05/06/21 11:00', age: 45 },
+  { id: 4, codigo: 'A1XXXXXXXXXXXXXXXE5', data: '05/06/21 11:00', age: 16 },
+  { id: 5, codigo: 'A1XXXXXXXXXXXXXXXE5', data: '05/06/21 11:00', age: null },
+  { id: 6, codigo: 'A1XXXXXXXXXXXXXXXE5', data: null, age: 150 },
+  { id: 7, codigo: 'A1XXXXXXXXXXXXXXXE5', data: '05/06/21 11:00', age: 44 },
+  { id: 8, codigo: 'A1XXXXXXXXXXXXXXXE5', data: '05/06/21 11:00', age: 36 },
+  { id: 9, codigo: 'A1XXXXXXXXXXXXXXXE5', data: '05/06/21 11:00', age: 65 },
+];
+
   const Item =(data)=> {
     console.log(data);
     return (
@@ -57,7 +86,7 @@ function App() {
  }
   return (
     < >
-      <table className="table">
+      {/* <table className="table">
       <caption>INFORMAÇÕES</caption>
           <thead>
             <tr>
@@ -69,7 +98,16 @@ function App() {
       </table>
       <tbody className="table">
       {dadosLength != null ?  <MyList/> : "Aguarde obtendo informações..." }
-      </tbody>
+      </tbody> */}
+      <div style={{ height: 400, width: '100%' }}>
+        <p style={{ height: 50, width: '100%', textAlign: 'center'}}>COMUNICANET</p>
+      <DataGrid
+        rows={dados}
+        columns={columns}
+        pageSize={5}
+        disableSelectionOnClick
+      />
+    </div>
     </>
   );
 }
